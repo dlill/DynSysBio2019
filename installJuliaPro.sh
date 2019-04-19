666a4076a432a147bb91830e8b8f37b4694bf838
Daniel Lill <daniel.lill@posteo.de>
2019-04-19 20:53:15 +0200

    update the installer

--- installJuliaPro.sh
+++ installJuliaPro.sh
@@ -1,3 +1,4 @@
-mkdir ~/JuliaPro
-chmod +x ~/Downloads/JuliaPro-1.0.3.1_build-24.sh 
-~/Downloads/JuliaPro-1.0.3.1_build-24.sh ~/JuliaProInstallation
+cd
+mkdir JuliaPro
+chmod +x ~/Downloads/JuliaPro-1.0.3.1_build-24.sh
+~/Downloads/JuliaPro-1.0.3.1_build-24.sh ~/JuliaPro
