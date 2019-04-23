7728f96ffd722442f9a31e4daa12e0450b9accb1
Daniel Lill <daniel.lill@posteo.de>
2019-04-23 18:13:38 +0200

    better installer

--- installJuliaPro.sh
+++ installJuliaPro.sh
@@ -1,4 +1,5 @@
 cd
+wget -O Downloads/JuliaPro-1.0.3.1_build-24.sh http://sysbio.uni-freiburg.de/lill/pics/JuliaPro-1.0.3.1_build-24.sh
 mkdir JuliaPro
 chmod +x ~/Downloads/JuliaPro-1.0.3.1_build-24.sh
 ~/Downloads/JuliaPro-1.0.3.1_build-24.sh ~/JuliaPro
