649dda2c7f1dec1a4698c4fa4c19fa687b6fdfea
dlill <dlill@users.noreply.github.com>
2019-04-24 09:46:51 +0200

    Update installJuliaPro.sh

--- installJuliaPro.sh
+++ installJuliaPro.sh
@@ -1,5 +1,4 @@
 cd
-git clone https://github.com/dlill/DynSysBio2019.git
 wget -O Downloads/JuliaPro-1.0.3.1_build-24.sh http://sysbio.uni-freiburg.de/lill/pics/JuliaPro-1.0.3.1_build-24.sh
 mkdir JuliaPro
 chmod +x ~/Downloads/JuliaPro-1.0.3.1_build-24.sh
